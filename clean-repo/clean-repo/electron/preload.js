eaebcd76ebc3ff06e86a85201f8eded6703f722e
Adi Gusi <adigusi@example.com>
2026-06-01 21:21:28 +0700

    Remove accidentally committed clean-repo directory

--- clean-repo/clean-repo/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  setAnimeActivity: (activity) => ipcRenderer.invoke('set-anime-activity', activity),
-  clearAnimeActivity: () => ipcRenderer.invoke('clear-anime-activity'),
-});
